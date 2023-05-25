04a9256c27c82acb61f01fe1e19e2cf4b55aa983
salvomc <smcrwrk@gmail.com>
2023-05-25 15:35:13 +0200

    palindromo js done

---
+++ palindromo/js/script.js
@@ -0,0 +1,21 @@
+
+//Prompt inserimento aprola da parte dell'utente
+let word = prompt('Inserisci una parola');
+
+//Funzione Split
+let arrayWord = word.split('');
+
+//Funzione Reverse
+let arrayReverseWord = arrayWord.reverse();
+
+//Funzione Join
+let reverseWord = arrayReverseWord.join('');
+
+//Controllo parola palindroma
+if (reverseWord === word) 
+    {
+        console.log('La parola inserita è palindroma.');
+    } else 
+        {
+            console.log('La parola inserita non è palindroma.');
+        }
